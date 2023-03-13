7432cc45207ec50f098feeac3ea16c2902ba20e3
Daniel Hanold <daniel.hanold@sitecompli.com>
2023-03-13 16:55:27 -0400

    Add actual change on file

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 require __DIR__ . '/vendor/autoload.php';
 use Carbon\Carbon;
 
+printf("This is a very basic output.<br><br>");
 
-printf("Right now in New York is %s.\n", Carbon::now('US/Eastern')->format('Y-m-d g:i:s A'));
+printf("Right now in New York is %s.<br>", Carbon::now('US/Eastern')->format('Y-m-d g:i:s A'));
 
-printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
+printf("Tomorrow in New York will be %s.<br>", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
 
-printf("Right now in Paris is %s\n", Carbon::now('Europe/Paris'));
+printf("Right now in Paris is %s.<br>", Carbon::now('Europe/Paris'));
